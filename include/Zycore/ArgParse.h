1f714371ae5eca57d3ee179e7ad32e58f24ef797
Joel Höner <athre0z@zyantific.com>
2019-07-13 14:59:34 +0200

    Document arg parsing

--- include/Zycore/ArgParse.h
+++ include/Zycore/ArgParse.h
@@ -53,25 +53,57 @@ typedef struct ZyanArgParseDefinition_
     /**
      * @brief   The argument name, e.g. `--help`.
      *
-     * Must start with either one or two dashes.
+     * Must start with either one or two dashes. Single dash arguments must consist of a single
+     * character, (e.g. `-n`), double-dash arguments can be of arbitrary length.
      */
     const char* name;
+    /**
+     * @brief   Whether the argument is boolean or expects a value.
+     */
     ZyanBool boolean;
 } ZyanArgParseDefinition;
 
 typedef struct ZyanArgParseConfig_
 {
+    /**
+     * @brief   `argv` argument passed to `main` by LibC.
+     */
     const char** argv;
+    /**
+     * @brief   `argc` argument passed to `main` by LibC.
+     */
     ZyanUSize argc;
+    /**
+     * @brief   Minimum # of accepted unnamed / anonymous arguments.
+     */
     ZyanUSize min_unnamed_args;
+    /**
+     * @brief   Maximum # of accepted unnamed / anonymous arguments.
+     */
     ZyanUSize max_unnamed_args;
+    /**
+     * @brief   Argument definition array, or `NULL`.
+     *
+     * Expects a pointer to an array of `ZyanArgParseDefinition` instances. The array is
+     * terminated by setting the `.name` field of the last element to `NULL`. If no named
+     * arguments should be parsed, you can also set this to `NULL`.
+     */
     ZyanArgParseDefinition* args;
 } ZyanArgParseConfig;
 
 typedef struct ZyanArgParseArg_
 {
-    const ZyanArgParseDefinition* arg;  // NULL = unnamed argument
+    /**
+     * @brief   Corresponding argument definition, or `NULL` for unnamed args.
+     */
+    const ZyanArgParseDefinition* arg;
+    /**
+     * @brief   Whether the argument has a value (is non-boolean).
+     */
     ZyanBool has_value;
+    /**
+     * @brief   If `has_value == true`, then the argument value.
+     */
     ZyanStringView value;
 } ZyanArgParseArg;
 
@@ -79,8 +111,15 @@ typedef struct ZyanArgParseArg_
 /* Exported functions                                                                             */
 /* ============================================================================================== */
 
+/**
+ * @brief  Parse arguments according to a `ZyanArgParseConfig` definition.
+ *
+ * @param  cfg      Argument parser config to use.
+ * @param  parsed   Receives the parsed output.
+ *
+ * @return A `ZyanStatus` status determining whether the parsing succeeded.
+ */
 ZyanStatus ZyanArgParse(const ZyanArgParseConfig *cfg, ZyanVector/*<ZyanArgParseArg>*/* parsed);
-ZyanStatus ZyanArgParseHelpText(const ZyanArgParseConfig *cfg, ZyanString* help_text);
 
 /* ============================================================================================== */
 
